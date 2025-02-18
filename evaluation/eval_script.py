98fd3be8679a1a2c456713f796a77675e542f475
dongxingMao <dongxingmao99@gmail.com>
2025-02-18 07:05:06 +0000

    remove wrong dataset_type

--- evaluation/eval_script.py
+++ evaluation/eval_script.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--output_dir", type=str, default="results", help="The directory to save the evaluationresults")
     parser.add_argument("--image_save_dir", type=str, default="generated_images", help="The directory to save the generated images")
-    parser.add_argument("--dataset_type", type=str, default="styledtextsynth", choices=["styledtextsynth", "textvisionblend", "textvision", "textsceneshq"])
+    parser.add_argument("--dataset_type", type=str, default="styledtextsynth", choices=["styledtextsynth", "textvisionblend", "textsceneshq"])
     parser.add_argument("--cal_ocr", action="store_true", help="Whether to calculate the OCR metrics")
     parser.add_argument("--cal_clip", action="store_true", help="Whether to calculate the CLIP metrics")
     parser.add_argument("--cal_fid", action="store_true", help="Whether to calculate the FID metrics")
